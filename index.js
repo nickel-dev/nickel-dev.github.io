9a75bb795e44e39349403ab96a22b98f38fcc65b
Nickel-Dev <92646300+nickel-dev@users.noreply.github.com>
2022-07-13 11:42:48 +0200

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-function toggleCursor(){
-  document.getElementById("cursor").style.color="transparent" === document.getElementById("cursor").style.color? "inherit":"transparent";
-}
-setInterval(toggleCursor,500)
